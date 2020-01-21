6fc264f4ff3185edc702d215a02215eae8c60b6d
Emre Karahan <emre.karahan@outlook.com>
2020-01-21 09:08:06 +0300

    build.cmd fixed

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
-rd /s /q build
-dotnet publish ../src/Valkyrie.Core/Valkyrie.Core.csproj -f netcoreapp2.0 -o ..\build -c Release
-dotnet publish ../src/Valkyrie.Core/Valkyrie.Core.csproj -f netcoreapp2.1 -o ..\build -c Release
+rd /s /q ./build
+dotnet publish src/Valkyrie.Core/Valkyrie.Core.csproj -f netstandard2.0 -o build/Valkyrie.Core/netstandard2.0 -c Release
+dotnet publish src/Valkyrie.Core/Valkyrie.Core.csproj -f netstandard2.1 -o build/Valkyrie.Core/netstandard2.1 -c Release
